6d0295add666939cfcfe611b68da2ea4ca0fdd32
Carl Andrew Linao <59546040+wolfgang04@users.noreply.github.com>
2024-12-03 02:32:54 +0800

    Update App.tsx to match main

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -8,7 +8,17 @@ function App() {
 	return (
 		<Router>
 		  <Routes>
-			<Route path="/" element={<Login />} />
+			<Route path="/" element={<Navbar />}>
+			        <Route index element={<Dashboard />} />
+			        <Route path="discovery" element={<Discovery />} />
+			        <Route path="discovery/genres" element={<Genres />} />
+			        <Route path="discovery/genres/:genre" element={<GenreSong />} />
+			        <Route path="community" element={<Community />} />
+			        <Route path="artist/:id" element={<Artist />} />
+			        <Route path="album/:id" element={<Album />} />
+			        <Route path="track/:id" element={<Track />} />
+     			</Route>
+			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<SignUp />} />
 			<Route path="/resetpass" element={<ResetPass />} />
 		  </Routes>
