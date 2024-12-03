13ba2a6fa58dd0463cb2e9ef9eaa0db3440a8911
Juliana Marie Ochea <julianamarieochea@gmail.com>
2024-12-03 20:30:08 +0800

    revised login & sign up ui

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,29 +1,37 @@
-import SampleComponent from "../components/SampleComponent";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "../components/Navbar/Navbar";
+import Dashboard from "../pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import Genres from "../pages/Discovery/Genres";
+import Discovery from "../pages/Discovery/Discovery";
+import GenreSong from "../pages/Discovery/GenreSong";
+import Artist from "../pages/Artist";
+import Album from "../pages/Album";
+import Track from "../pages/Track";
+import Community from "../pages/Community";
+import Settings from "../pages/Settings";
 import Login from '../pages/login';
 import SignUp from '../pages/signup';
 import ResetPass from '../pages/resetpass';
 
 function App() {
-	return (
-		<Router>
-		  <Routes>
-			<Route path="/" element={<Navbar />}>
-			        <Route index element={<Dashboard />} />
-			        <Route path="discovery" element={<Discovery />} />
-			        <Route path="discovery/genres" element={<Genres />} />
-			        <Route path="discovery/genres/:genre" element={<GenreSong />} />
-			        <Route path="community" element={<Community />} />
-			        <Route path="artist/:id" element={<Artist />} />
-			        <Route path="album/:id" element={<Album />} />
-			        <Route path="track/:id" element={<Track />} />
-     			</Route>
-			<Route path="/login" element={<Login />} />
-			<Route path="/signup" element={<SignUp />} />
-			<Route path="/resetpass" element={<ResetPass />} />
-		  </Routes>
-		</Router>
-	  );
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Dashboard />} />
+        <Route path="discovery" element={<Discovery />} />
+        <Route path="discovery/genres" element={<Genres />} />
+        <Route path="discovery/genres/:genre" element={<GenreSong />} />
+        <Route path="community" element={<Community />} />
+        <Route path="artist/:id" element={<Artist />} />
+        <Route path="album/:id" element={<Album />} />
+        <Route path="track/:id" element={<Track />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+	    <Route path="/login" element={<Login />} />
+		<Route path="/signup" element={<SignUp />} />
+		<Route path="/resetpass" element={<ResetPass />} />
+    </Routes>
+  );
 }
 
 export default App;
