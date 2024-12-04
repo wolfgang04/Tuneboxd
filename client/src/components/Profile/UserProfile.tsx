6d89e7629477176dd5359d7eee18519d31cf6382
Andrew <carlandrewlinao@gmail.com>
2024-12-04 08:12:17 +0800

    chore: code cleanup

--- client/src/components/Profile/UserProfile.tsx
+++ client/src/components/Profile/UserProfile.tsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import { User } from "../../pages/Profile";
 
 interface UserProfileProps {
   isCurrentUser: boolean;
+  details: User;
 }
 
-const UserProfile: React.FC<UserProfileProps> = ({ isCurrentUser }) => {
+const UserProfile: React.FC<UserProfileProps> = ({
+  isCurrentUser,
+  details,
+}) => {
   const [isFollowing, setIsFollowing] = useState(false);
   const [followerCount, setFollowerCount] = useState(234);
 
   const handleFollowToggle = () => {
     setIsFollowing(!isFollowing);
-    setFollowerCount((prevCount) => (isFollowing ? prevCount - 1 : prevCount + 1));
+    setFollowerCount((prevCount) =>
+      isFollowing ? prevCount - 1 : prevCount + 1,
+    );
   };
 
   return (
@@ -18,10 +25,10 @@ const UserProfile: React.FC<UserProfileProps> = ({ isCurrentUser }) => {
       <img
         src="https://simplyilm.com/wp-content/uploads/2017/08/temporary-profile-placeholder-1.jpg"
         alt="Profile Picture"
-        className="w-[200px] h-[200px] bg-gray-300 rounded-full mb-4"
+        className="mb-4 h-[200px] w-[200px] rounded-full bg-gray-300"
       />
-      <h1 className="text-xl font-bold text-center">Username</h1>
-      <div className="flex justify-center mt-4 w-full text-center gap-x-12">
+      <h1 className="text-center text-xl font-bold">{details.username}</h1>
+      <div className="mt-4 flex w-full justify-center gap-x-12 text-center">
         <div>
           <p className="font-bold">{followerCount}</p>
           <p className="text-sm text-gray-600">Followers</p>
@@ -31,12 +38,12 @@ const UserProfile: React.FC<UserProfileProps> = ({ isCurrentUser }) => {
           <p className="text-sm text-gray-600">Following</p>
         </div>
       </div>
-      {!isCurrentUser && ( 
+      {!isCurrentUser && (
         <button
           onClick={handleFollowToggle}
-          className={mt-6 py-2 px-6 rounded ${
+          className={`mt-6 rounded px-6 py-2 ${
             isFollowing ? "bg-black text-white" : "bg-black text-white"
-          }}
+          }`}
         >
           {isFollowing ? "Unfollow" : "Follow"}
         </button>
