42de29347c38c5de1b2b1d9b3dca9960e8ec484a
Guest Linao <carlandrewlinao@gmail.com>
2024-12-17 18:36:07 +0800

    update for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import app from "./app";
 import { PORT } from "./constants";
 
+app.use("/", (req, res) => {
+	res.send("Server is running");
+});
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
