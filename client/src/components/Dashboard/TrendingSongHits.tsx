dc0af9dfa578b9830425e93d9df1f6ee125d3a36
Andrew <carlandrewlinao@gmail.com>
2025-03-05 13:25:41 +0800

    push for deployment

--- client/src/components/Dashboard/TrendingSongHits.tsx
+++ client/src/components/Dashboard/TrendingSongHits.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface TrendingSong {
   rank: string;
@@ -11,33 +11,59 @@ interface TrendingSong {
   song: string;
 }
 
-const TrendingSongItem: React.FC<TrendingSong> = ({ rank, title, artist, imageUrl, artistId, songId, song }) => {
-  const navigate = useNavigate();
-
-  return <div
-    className="flex flex-col flex-1 items-start py-3.5 pr-12 pl-5 rounded-3xl bg-cover bg-center bg-no-repeat max-md:pr-5 cursor-pointer"
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    onClick={() => navigate(`/track/${songId}`, { state: song  })}
-  >
-    <div data-layername={rank} className="text-4xl text-white bg-black/50 p-2 rounded-md">{`#${rank}`}</div>
-    <div data-layername="title" className="mt-28 text-20 font-bold text-white max-md:mt-10 bg-black/50 p-2 rounded-md">{title}</div>
-    <div data-layername="artistName" className="text-base text-white bg-black/50 p-2 rounded-md hover:underline"
-    onClick={(e) => {
-      e.stopPropagation();
-      navigate(`/artist/${artistId}`, {state: artist});
-    }}>{artist}</div>
-  </div>
-};
-
-const TrendingSongHits: React.FC<{ trendingSongs: TrendingSong[] }> = ({ trendingSongs }) => {
+const TrendingSongItem: React.FC<TrendingSong> = ({
+  rank,
+  title,
+  artist,
+  imageUrl,
+  artistId,
+  songId,
+  song,
+}) => {
   const navigate = useNavigate();
 
   return (
+    <div
+      className="flex flex-1 cursor-pointer flex-col items-start rounded-3xl bg-cover bg-center bg-no-repeat py-3.5 pl-5 pr-12 max-md:pr-5"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      onClick={() => navigate(`/track/${songId}`, { state: song })}
+    >
+      <div
+        data-layername={rank}
+        className="rounded-md bg-black/50 p-2 text-4xl text-white"
+      >{`#${rank}`}</div>
+      <div
+        data-layername="title"
+        className="text-20 mt-28 rounded-md bg-black/50 p-2 font-bold text-white max-md:mt-10"
+      >
+        {title}
+      </div>
+      <div
+        data-layername="artistName"
+        className="rounded-md bg-black/50 p-2 text-base text-white hover:underline"
+        onClick={(e) => {
+          e.stopPropagation();
+          navigate(`/artist/${artistId}`, { state: artist });
+        }}
+      >
+        {artist}
+      </div>
+    </div>
+  );
+};
+
+const TrendingSongHits: React.FC<{ trendingSongs: TrendingSong[] }> = ({
+  trendingSongs,
+}) => {
+  return (
     <section className="mt-16 max-md:mt-10">
-      <h2 data-layername="trendingSongHits" className="self-start ml-10 text-4xl font-bold text-black max-md:ml-2.5">
+      <h2
+        data-layername="trendingSongHits"
+        className="ml-10 self-start text-4xl font-bold text-black max-md:ml-2.5"
+      >
         Trending Song Hits
       </h2>
-      <div className="flex flex-wrap gap-2.5 self-center mt-5 w-full text-white max-w-[1520px] max-md:max-w-full">
+      <div className="mt-5 flex w-full max-w-[1520px] flex-wrap gap-2.5 self-center text-white max-md:max-w-full">
         {trendingSongs.map((song) => (
           <TrendingSongItem key={song.rank} {...song} />
         ))}
